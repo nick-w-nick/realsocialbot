3ce445af8958a1d37c4200e94951ee26389e8c54
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2022-08-11 20:56:13 -0400

    added install utils + ignoreUndefinedProperties flag for install docs

--- utils/firestore.js
+++ utils/firestore.js
@@ -3,6 +3,8 @@ import { getFirestore } from 'firebase-admin/firestore';
 import init from './init.js';
 
 await init();
+
 const firestore = getFirestore();
+firestore.settings({ ignoreUndefinedProperties: true });
 
 export default firestore;
